76aca8fca44e4b249b4a534fb4fe733e7e1b5cb0
Diego Herrera Gre <diego.herrera.94@outlook.com>
2024-07-10 21:02:13 -0400

    Nueva versión de la entrega final DHGBackendI

---
+++ src/config/mongoDB.config.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectMongoDB = async () => {
+  try {
+      mongoose.connect("mongodb+srv://diegoherreragre:K97ZkJZQJrBGuMZd@dbdhg.cum2t4l.mongodb.net/")
+      console.log("MongoDB connected");
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+}
